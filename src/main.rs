0b1c876e2b877e391c73a0c8c2987010e951b0d5
Charalampos Fanoulis <yo@fanoulis.dev>
2022-06-09 22:34:53 +0300

    refactor: idk how to describe this honestly

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,11 @@ extern crate tfc;
 use std::{thread, time::Duration};
 use tfc::{Context, traits::*};
 
-#[post("/", data = "<paste>")]
-async fn receive(paste: String) -> &'static str {
-    println!("I got {}", paste);
+#[post("/", data = "<d>")]
+async fn receive(d: String) -> &'static str {
+    println!("I got {}", d);
+
+    if (d.len() == 0) { return "Man that's null" }
 
     let mut ctx = match Context::new() {
         Ok(ctx) => ctx,
@@ -15,10 +17,12 @@ async fn receive(paste: String) -> &'static str {
 
     // For OS-specific reasons, it's necessary to wait a moment after
     // creating the context before generating events.
-    thread::sleep(Duration::from_millis(15));
-    ctx.unicode_string("321");
+    thread::sleep(Duration::from_millis(950));
+    match ctx.ascii_string(d.as_bytes()) {
+        Ok(_) => "Thanks man",
+        Err(_) => "Couldn't type"
+    }
 
-    "Thanks man"
 }
 
 #[get("/")]
