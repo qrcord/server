da893a1f8fe346935ecd0adb0da06b829684724c
Charalampos Fanoulis <yo@fanoulis.dev>
2022-06-11 20:48:46 +0300

    refactor: use proper structs for responses

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
+
 #[macro_use] extern crate rocket;
 extern crate tfc;
 
+use rocket::{response::{status, content}, http::Status};
 use std::{thread, time::Duration};
 use tfc::{Context, traits::*};
 
 #[post("/", data = "<d>")]
-async fn receive(d: String) -> &'static str {
+async fn receive(d: String) -> Result<status::Accepted<&'static str>, status::Custom<content::RawText<&'static str>>>  {
     println!("I got {}", d);
 
-    if (d.len() == 0) { return "Man that's null" }
+    if d.len() == 0 { return Err(status::Custom(Status::BadRequest, content::RawText("No data"))) }
 
     let mut ctx = match Context::new() {
         Ok(ctx) => ctx,
@@ -17,10 +19,10 @@ async fn receive(d: String) -> &'static str {
 
     // For OS-specific reasons, it's necessary to wait a moment after
     // creating the context before generating events.
-    thread::sleep(Duration::from_millis(950));
+    thread::sleep(Duration::from_millis(350));
     match ctx.ascii_string(d.as_bytes()) {
-        Ok(_) => "Thanks man",
-        Err(_) => "Couldn't type"
+        Ok(_) => Ok(status::Accepted(Some(""))),
+        Err(_) => Err(status::Custom(Status::BadRequest, content::RawText("Bad data")))
     }
 
 }
